bb1adae566dcc529bde922f6c06933183dddc73f
MRFI <rijalfadliislamy03@gmail.com>
2026-07-17 15:50:13 +0700

    added the gold.fact_customer_health code, tested, and validated directly inside the duckdb

---
+++ pipelines/gold/sql/fact_customer_health.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE TABLE gold.fact_customer_health AS
+WITH
+customers AS (
+  SELECT DISTINCT customer_key, MAX((order_ts_raw AT TIME ZONE 'UTC') AT TIME ZONE 'America/New_York') AS last_order_date
+  FROM silver.stg_orders
+  GROUP BY customer_key
+),
+reference_point AS (
+  SELECT MAX(last_order_date) AS reference_date FROM customers
+),
+customer_health_reshaped AS (
+  SELECT 
+    s.customer_key,
+    r.customer_type,
+    s.last_order_date,
+    CAST(rp.reference_date AS DATE) - CAST(s.last_order_date AS DATE) AS days_since_last_order,
+    CASE
+      WHEN days_since_last_order >= 60 THEN 'churned'
+      WHEN days_since_last_order >= 45 THEN 'at_risk'
+      ELSE 'active'
+    END AS customer_health
+  FROM customers s
+  CROSS JOIN reference_point rp
+  LEFT JOIN silver.stg_customers r
+    ON s.customer_key = r.customer_key
+  WHERE customer_type IN ('loyalty', 'online')
+)
+SELECT * FROM customer_health_reshaped
+--tested manually inside the duckdb notebook
---
+++ pipelines/gold/sql/fact_subscription_churn.sql
@@ -0,0 +1,22 @@
+
+CREATE OR REPLACE TABLE gold.fact_subscription_churn AS
+WITH
+table_partition AS (
+  SELECT *
+  FROM (
+      SELECT *,
+          ROW_NUMBER() OVER (PARTITION BY customer_key ORDER BY subscription_created_at DESC) AS rn
+      FROM silver.stg_subscriptions
+  )
+  WHERE rn = 1 
+)
+SELECT 
+  subscription_id, customer_key,
+  CASE
+    WHEN customer_status = 'is_voluntarily_cancelled' THEN 'is_voluntarily_churned'
+    WHEN customer_status = 'is_payment_failure_cancelled' THEN 'is_involuntarily_churned'
+    WHEN customer_status IN ('is_active', 'is_paused') THEN 'not_churned'
+    ELSE NULL
+  END AS churn_status
+FROM table_partition;
+--tested manually inside the database
