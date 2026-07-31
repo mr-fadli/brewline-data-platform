eb4c310161e69492ae66faac8206bb095e2793ab
MRFI <rijalfadliislamy03@gmail.com>
2026-07-31 13:17:10 +0700

    port fact_customer_health by adding more CTE to adjust with the bigquery's standard and adding more var for easier test and maintenance in the dbt_project.yml

--- brewline_dbt/models/marts/fact_customer_health.sql
+++ brewline_dbt/models/marts/fact_customer_health.sql
@@ -1,28 +1,42 @@
--- models/marts/fact_customer_health.sql
+{{
+  config(
+    materialized='table'
+  )
+}}
+
 WITH
 customers AS (
-  SELECT DISTINCT customer_key,
-      MAX((order_ts_raw AT TIME ZONE 'UTC') AT TIME ZONE 'America/New_York') AS last_order_date
-  FROM {{ ref('stg_orders') }}
-  GROUP BY customer_key
+    SELECT
+        customer_key,
+        MAX({{ convert_utc_to_local('order_ts_raw', "'" ~ var('reporting_timezone') ~ "'") }}) AS last_order_date
+    FROM {{ ref('stg_orders') }}
+    GROUP BY customer_key
 ),
 reference_point AS (
-  SELECT MAX(last_order_date) AS reference_date FROM customers
+    SELECT MAX(last_order_date) AS reference_date FROM customers
+),
+days_calc AS (
+    SELECT
+        s.customer_key,
+        r.customer_type,
+        s.last_order_date,
+        {{ dbt.datediff('CAST(s.last_order_date AS DATE)', 'CAST(rp.reference_date AS DATE)', 'day') }} AS days_since_last_order
+    FROM customers s
+    CROSS JOIN reference_point rp
+    LEFT JOIN {{ ref('stg_customers') }} r ON s.customer_key = r.customer_key
+    WHERE r.customer_type IN ('loyalty', 'online')
 ),
 customer_health_reshaped AS (
-  SELECT 
-    s.customer_key,
-    r.customer_type,
-    s.last_order_date,
-    CAST(rp.reference_date AS DATE) - CAST(s.last_order_date AS DATE) AS days_since_last_order,
-    CASE
-      WHEN days_since_last_order >= 60 THEN 'churned'
-      WHEN days_since_last_order >= 45 THEN 'at_risk'
-      ELSE 'active'
-    END AS customer_health
-  FROM customers s
-  CROSS JOIN reference_point rp
-  LEFT JOIN {{ ref('stg_customers') }} r ON s.customer_key = r.customer_key
-  WHERE customer_type IN ('loyalty', 'online')
+    SELECT
+        customer_key,
+        customer_type,
+        last_order_date,
+        days_since_last_order,
+        CASE
+            WHEN days_since_last_order >= {{ var('customer_churn_days_threshold') }} THEN 'churned'
+            WHEN days_since_last_order >= {{ var('customer_at_risk_days_threshold') }} THEN 'at_risk'
+            ELSE 'active'
+        END AS customer_health
+    FROM days_calc
 )
 SELECT * FROM customer_health_reshaped
